61d046d40680e2b035aebb06b720d3db95ea64a7
yunusbahtiar22 <aboomarmoshab22@gmail.com>
2022-06-10 06:28:13 +0700

    feat: Add new functionality

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+
+   const list = [...theaterIXX, ...theaterVGC];
+   let total = 0;
+   list.forEach(item => {
+     const { hasil } = item;
+     hasil === emosi ? total++ : total + 0;
+   });
+
+  return total;
+
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
